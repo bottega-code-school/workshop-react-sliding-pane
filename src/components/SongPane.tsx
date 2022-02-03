eb0f8eeef030c12d12bf4f394f3ca039842fd3f4
Jordan Hudgens <jordan@devcamp.com>
2022-02-03 12:24:21 -0700

    Updated final version

--- src/components/SongPane.tsx
+++ src/components/SongPane.tsx
@@ -19,22 +19,24 @@ export default function SongPane({ visible, data, closePane }: Props) {
       onRequestClose={closePane}
       hideHeader
     >
-      <div className="song-details">
-        <div className="song-details__thumb">
-          <img src={data?.thumb} />
-        </div>
-        <div className="song-details__info">
-          <a onClick={closePane} className="song-details__info--close">
-            <BsCaretLeft />
-            CLOSE
-          </a>
-          <div className="song-details__info--box">
-            <div>{data?.artist}</div>
-            <div className="bold">{data?.released}</div>
+      {data && (
+        <div className="song-details">
+          <div className="song-details__thumb">
+            <img src={data?.thumb} />
           </div>
-          <div>{data?.description}</div>
+          <div className="song-details__info">
+            <a onClick={closePane} className="song-details__info--close">
+              <BsCaretLeft />
+              CLOSE
+            </a>
+            <div className="song-details__info--box">
+              <div>{data?.artist}</div>
+              <div className="bold">{data?.released}</div>
+            </div>
+            <div>{data?.description}</div>
+          </div>
         </div>
-      </div>
+      )}
     </SlidingPane>
   );
 }
